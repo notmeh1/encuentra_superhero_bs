26acff4933f977dffeadd747b0e828cb2388ca2b
Andres <zleckzxd@gmail.com>
2021-07-16 01:33:02 -0400

    js part 1 done

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-"use strict"
+"use strict";
 
-const heroInput = $("#input")
-const heroSubmit = $("#submit")
-const heroInfo = $("#hero-data")
+const heroInput = $("#input");
+const heroSubmit = $("#submit");
+const heroInfo = $("#hero-data");
+const heroError = $("#heroError");
 
-$("#submit").click(function(event) {
-    event.preventDefault();
-    $("#input").submit()
-    $.ajax({
-        type : 'GET',
-        dataType : 'json',
-        url : `https://www.superheroapi.com/api.php/1104494366627102/`+ $("#input").val(),
-        success: function(data) {
-            $("hero-data").text(data)
-            const dataReady = JSON.stringify(data)
-            console.log(dataReady)
-            $("#hero-data").html (dataReady)
-        }
-    })
-    console.log($("input").val())
-})
+/* Encontrar porque la condicional no funciona */
+
+heroSubmit.click(function (event) {
+  event.preventDefault();
+  if (heroInput.val() > 731) {
+    heroError.html("El ID está fuera de rango");
+  } else if (heroInput.val() == 0) {
+    heroError.html("Ingrese un ID válido");
+  } else {
+    console.log(heroInput.val());
+    heroInput.submit();
+    fetchHeroData();
+  }
+});
+
+function fetchHeroData() {
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url:
+      `https://www.superheroapi.com/api.php/1104494366627102/` +
+      $("#input").val(),
+    success: function (data) {
+      heroPrint(data);
+    },
+  });
+}
+
+function heroPrint(data) {
+  const heroImage = $("#heroImage");
+  const heroName = $("#heroName");
+  const heroGroupAffiliation = $("#group_affiliation");
+  const heroPublisher = $("#publisher");
+  const heroOccupation = $("#occupation");
+  const heroFirstAppearance = $("#first_appearance");
+  const heroHeight = $("#height");
+  const heroWeight = $("#weight");
+  const heroAliases = $("#aliases");
+  heroImage.attr("src", data.image.url);
+  heroName.html(`Nombre: ${data.name}`);
+  heroGroupAffiliation.html(
+    `Conexiones: ${data.connections["group-affiliation"]}`
+  );
+  heroPublisher.html(`Publicado por: ${data.biography.publisher}`);
+  heroOccupation.html(`Ocupación: ${data.work.occupation}`);
+  heroFirstAppearance.html(
+    `Primera Aparición: ${data.biography["first-appearance"]}`
+  );
+  heroHeight.html(`Altura: ${data.appearance.height.join(" - ")}`);
+  heroWeight.html(`Peso: ${data.appearance.weight.join(" - ")}`);
+  heroAliases.html(`Alianzas: ${data.biography.aliases.join(", ")}`);
+}
