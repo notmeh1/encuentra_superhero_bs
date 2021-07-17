c794c223dac624234284fc0058e88bdf98ee5f18
Andres <zleckzxd@gmail.com>
2021-07-17 00:15:49 -0400

    rest

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function fetchHeroData() {
     success: function (data) {
       if (data.response === "success"){
         heroPrint(data)
+        renderSuperheroChart(data)
         console.log("SUCCESS")
     } else {
       heroError.html("Ingrese un ID válido");
@@ -41,16 +42,18 @@ function fetchHeroData() {
 }
 
 function heroPrint(data) {
-  const heroImage = $("#heroImage");
-  const heroName = $("#heroName");
-  const heroGroupAffiliation = $("#group_affiliation");
-  const heroPublisher = $("#publisher");
-  const heroOccupation = $("#occupation");
-  const heroFirstAppearance = $("#first_appearance");
-  const heroHeight = $("#height");
-  const heroWeight = $("#weight");
-  const heroAliases = $("#aliases");
+  let heroImage = $("#heroImage");
+  const heroMsg = $("#heroFoundMsg")
+  let heroName = $("#heroName");
+  let heroGroupAffiliation = $("#group_affiliation");
+  let heroPublisher = $("#publisher");
+  let heroOccupation = $("#occupation");
+  let heroFirstAppearance = $("#first_appearance");
+  let heroHeight = $("#height");
+  let heroWeight = $("#weight");
+  let heroAliases = $("#aliases");
   heroImage.attr("src", data.image.url);
+  heroMsg.html(`SuperHero Encontrado`)
   heroName.html(`Nombre: ${data.name}`);
   heroGroupAffiliation.html(
     `Conexiones: ${data.connections["group-affiliation"]}`
@@ -63,5 +66,43 @@ function heroPrint(data) {
   heroHeight.html(`Altura: ${data.appearance.height.join(" - ")}`);
   heroWeight.html(`Peso: ${data.appearance.weight.join(" - ")}`);
   heroAliases.html(`Alianzas: ${data.biography.aliases.join(", ")}`);
+}
+  function renderSuperheroChart(data) {
+    const options = {
+      title: {
+        text: `Estadísitcas de poder para ${data.name}`,
+      },
+      data: [
+        {
+          type: "pie",
+          dataPoints: [
+            {
+              label: "Inteligencia",
+              y: Number.parseInt(data.powerstats.intelligence),
+            },
+            {
+              label: "Fuerza",
+              y: Number.parseInt(data.powerstats.strength),
+            },
+            {
+              label: "Velocidad",
+              y: Number.parseInt(data.powerstats.speed),
+            },
+            {
+              label: "Durabilidad",
+              y: Number.parseInt(data.powerstats.durability),
+            },
+            {
+              label: "Poder",
+              y: Number.parseInt(data.powerstats.power),
+            },
+            {
+              label: "Combate",
+              y: Number.parseInt(data.powerstats.combat),
+            },
+          ],
+        },
+      ],
+    };
+    $("#chartContainer").CanvasJSChart(options);
   }
- 
